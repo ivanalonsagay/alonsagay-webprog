b656842a8389abaa1b0b35b075b99dfc35117ba0
ivanalonsagay <ivanmalonsagay@gmail.com>
2026-05-26 16:09:56 +0800

    updated app route

--- alonsagay-client/src/App.jsx
+++ alonsagay-client/src/App.jsx
@@ -1,34 +1,19 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-// Main Website Layout
+// HomePage Structure
 import Layout from './components/Layout';
-
-// Dashboard Layout
-import DashLayout from './layouts/DashLayout';
-
-// Landing Pages
-import HomePage from './pages/LandingPages/HomePage';
-import AboutPage from './pages/LandingPages/AboutPage';
-import ArticleListPage from './pages/LandingPages/ArticleListPage';
-import ArticlePage from './pages/LandingPages/ArticlePage';
-
-// Auth Pages
-import SignInPage from './pages/AuthPages/SignInPage';
-import SignUpPage from './pages/AuthPages/SignUpPage';
-
-// Dashboard Pages
-import DashboardPage from './pages/DashboardPages/DashboardPage';
-import ReportsPage from './pages/DashboardPages/ReportsPage';
-import UsersPage from './pages/DashboardPages/UsersPage';
-
-// Other Pages
+import ArticlePage from './pages/ArticlePage';
+import ArticleList from './components/ArticleList';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
 import NotFoundPage from './pages/NotFoundPage';
+import SignInPage from './pages/SignInPage';
+import SignUpPage from './pages/SignUpPage';
 
 const routes = [
   {
     path: '/',
     element: <Layout />,
-    errorElement: <NotFoundPage />,
     children: [
       {
         path: '',
@@ -40,11 +25,11 @@ const routes = [
       },
       {
         path: 'articles',
-        element: <ArticleListPage />,
+        element: <ArticlePage />,
       },
       {
         path: 'articles/:id',
-        element: <ArticlePage />,
+        element: <ArticleList />,
       },
       {
         path: '*',
@@ -59,25 +44,6 @@ const routes = [
   {
     path: '/signup',
     element: <SignUpPage />,
-  },
-  {
-    path: '/dashboard',
-    element: <DashLayout />,
-    errorElement: <NotFoundPage />,
-    children: [
-      {
-        path: '',
-        element: <DashboardPage />,
-      },
-      {
-        path: 'reports',
-        element: <ReportsPage />,
-      },
-      {
-        path: 'users',
-        element: <UsersPage />,
-      },
-    ],
   },
 ];
 
